6e3c01e3488771c4a90d697e5db62b3837d3a149
Yasmin Alves <69992737+yasminalve@users.noreply.github.com>
2020-08-22 20:24:39 -0300

    Create 12.Vector-Sort.cpp

---
+++ 12.Vector-Sort.cpp
@@ -0,0 +1,22 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int n, temp;
+    vector <int> vet;
+    cin >> n;
+    while(n--) {
+        cin >> temp;
+        vet.push_back(temp);
+    }
+    sort (vet.begin(), vet.end());
+    for (auto numero: vet) {
+        cout << numero << " ";
+    }
+    return 0;
+}
